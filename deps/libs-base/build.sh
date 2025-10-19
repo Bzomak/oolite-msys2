aa4056a77393a8c2607dbfe1142e7b78fe5cecf8
Robert Horswell <r.j.horswell@googlemail.com>
2025-10-19 11:54:48 +0100

    Try building latest libsbase version 1.31.1

--- deps/libs-base/build.sh
+++ deps/libs-base/build.sh
@@ -13,6 +13,6 @@
 . /mingw64/share/GNUstep/Makefiles/GNUstep.sh
 cd libs-base || exit
 # Use OpenStep plist format
-sed -i '330 s/NSPropertyListXMLFormat_v1_0/NSPropertyListOpenStepFormat/' Source/NSUserDefaults.m
+sed -i '336 s/NSPropertyListXMLFormat_v1_0/NSPropertyListOpenStepFormat/' Source/NSUserDefaults.m
 ./configure --disable-xslt
 make -j "$(nproc)"
