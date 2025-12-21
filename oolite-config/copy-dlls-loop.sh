83b54b5f24b021e6c45770d29b0db074c60d4c79
Robert Horswell <r.j.horswell@googlemail.com>
2025-12-21 18:51:55 +0000

    Add MSYS2 environment detection to the cop-dlls-loop.sh script

--- oolite-config/copy-dlls-loop.sh
+++ oolite-config/copy-dlls-loop.sh
@@ -4,22 +4,34 @@
 #
 # Copy the required dlls to the oolite.app folder
 #
-# Usage: ./copy-dlls-loop.sh [application_name]
+# Usage: ./copy-dlls-loop.sh [application_name] [MINGW64|CLANG64]
 #
 # The script expects to be called by oolite-config/build.sh, being run from the root of the oolite-msys2 repository.
 # It expects Oolite to have been built.
 #
 ###############################
 
-# Check if the required argument is provided
-if [ $# -ne 1 ]; then
-    echo "Usage: $0 <application_name>"
+# Check if the required arguments are provided
+if [ $# -ne 2 ]; then
+    echo "Usage: $0 <application_name> <MINGW64|CLANG64>"
     exit 1
 fi
 
 # Store the application name provided as argument
 app_name=$1
 app_location="$(dirname "$app_name")/"
+
+msystem=$2
+build_system=""
+
+if [ "$msystem" = "MINGW64" ]; then
+    build_system="mingw64"
+elif [ "$msystem" = "CLANG64" ]; then
+    build_system="clang64"
+else
+    echo "Invalid or missing MSYSTEM argument. Use MINGW64 or CLANG64."
+    exit 1
+fi
 
 # Initialize variables for loop control
 iteration=1
@@ -37,15 +49,15 @@ while [ $iteration -le $max_iterations ]; do
     dll_list=$(ldd "$app_name")
     echo "Checking DLLs for $app_name..."
     
-    # Filter the DLLs by those in /mingw64/bin
-    filtered_dll_list=$(echo "$dll_list" | grep "/mingw64/bin" | awk '{print $3}' | sort | uniq)
+    # Filter the DLLs by those in /$build_system/bin
+    filtered_dll_list=$(echo "$dll_list" | grep "/$build_system/bin" | awk '{print $3}' | sort | uniq)
     
     if [ -z "$filtered_dll_list" ]; then
-        echo "No DLLs found in directory: /mingw64/bin"
+        echo "No DLLs found in directory: /$build_system/bin"
         break
     fi
     
-    echo "DLLs in /mingw64/bin required by $app_name:"
+    echo "DLLs in /$build_system/bin required by $app_name:"
     echo "$filtered_dll_list"
     
     # Check if this is the same as the previous iteration
