75da8f9f7b983aa3a6747ffad5f47fc90aaf6ae7
Robert Horswell <r.j.horswell@googlemail.com>
2025-12-22 20:25:39 +0000

    Shouldn't need to call the GNUstep.sh script when making the installers, nor have any environment differences in the installer build script.

--- oolite-installer-config/build.sh
+++ oolite-installer-config/build.sh
@@ -4,7 +4,7 @@
 #
 # Configure and build the Oolite installers
 #
-# Usage: ./build.sh [release|release-deployment|release-snapshot] [MINGW64|CLANG64]
+# Usage: ./build.sh [release|release-deployment|release-snapshot]
 #
 # The script expects to be run from the root of the oolite-msys2 repository.
 # It expects tools-make to be installed, and for oolite to be downloaded and built.
@@ -13,17 +13,4 @@
 
 cd oolite || exit
 
-msystem=$2
-build_system=""
-if [ "$msystem" = "MINGW64" ]; then
-    build_system="mingw64"
-elif [ "$msystem" = "CLANG64" ]; then
-    build_system="clang64"
-else
-    echo "Invalid or missing MSYSTEM argument. Use MINGW64 or CLANG64."
-    exit 1
-fi
-
-# shellcheck source=/dev/null
-. /$build_system/share/GNUstep/Makefiles/GNUstep.sh
 make -j "$(nproc)" -f Makefile pkg-win-"$1"
